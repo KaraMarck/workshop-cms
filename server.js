05a6921fec190e041c984130b4b892513a8fc9de
Kara de la Marck <karadelamarck@gmail.com>
2016-02-27 16:23:50 +0000

    step 6

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var querystring = require('querystring');
 
 var message = 'I am so happy to be part of the Node Girls workshop!';
 var nodeMessage = 'Node! '
@@ -9,7 +10,7 @@ function handler (request, response) {
     var endpoint = request.url;
     if (endpoint === '/') {
         response.writeHead(200, {"Content-Type": "text/html"});
-        fs.readFile(__dirname + '/public/index.html', function(error, file) {
+        fs.readFile(__dirname + '/assets/index.html', function(error, file) {
             if (error) {
                 console.log(error);
                 return;
@@ -29,6 +30,19 @@ function handler (request, response) {
         response.write(message);
         response.end();
     }
+    else if (endpoint === '/create-post') {
+        var allTheData = '';
+        request.on('data', function (chunkOfData) {
+            allTheData += chunkOfData;
+        });
+
+        request.on('end', function () {
+            var convertedData = querystring.parse(allTheData);
+            console.log(convertedData);
+             response.writeHead(301, {"Location": '/'});
+            response.end();
+        });
+    }
     else {
         if (endpoint.indexOf('css') > 0) {
             response.writeHead(200, {"Content-Type": "text/css"});
@@ -36,7 +50,7 @@ function handler (request, response) {
         else if (endpoint.indexOf('jpg') > 0) {
             response.writeHead(200, {"Content-Type": "image/png"});
         }
-        fs.readFile(__dirname + '/public' + endpoint, function(error, file) {
+        fs.readFile(__dirname + '/assets' + endpoint, function(error, file) {
             if (error) {
                 console.log(error);
                 return;
@@ -50,4 +64,3 @@ var server = http.createServer(handler);
 server.listen(3000, function() {
     console.log("Server is listening on port 3000.  Ready to accept requests!")
 });
-
