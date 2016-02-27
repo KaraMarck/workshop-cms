820c0b02bef5caba88cd516f34592d530704f141
Kara de la Marck <karadelamarck@gmail.com>
2016-02-27 13:18:31 +0000

    step 4

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
-console.log('hello here!');
-
 var http = require('http');
+var fs = require('fs');
 
 var message = 'I am so happy to be part of the Node Girls workshop!';
+var nodeMessage = 'Node! '
+var girlsMessage = 'Node Girls! '
 
 function handler (request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(message);
-    response.end();
+    var endpoint = request.url;
+    if (endpoint === '/') {
+        response.writeHead(200, {"Content-Type": "text/html"});
+        fs.readFile(__dirname + '/public/index.html', function(error, file) {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            response.end(file);
+        });
+    }
+    else {
+       response.writeHead(200, {"Content-Type": "text/html"});
+    if (endpoint == '/node') {
+       response.write(nodeMessage);
+    }
+   if (endpoint == '/girls') {
+       response.write(girlsMessage);
+   }
+   response.write(message);
+   response.end();
+   }
 }
 var server = http.createServer(handler);
 
